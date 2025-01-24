f09cd67cb2e8628f20e06a3a1e9013a847da98c6
DennyNo1 <zht2041359@vip.qq.com>
2025-01-24 15:13:42 +0800

    add authentication

--- back-end/database.js
+++ back-end/database.js
@@ -2,8 +2,19 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 async function connectToDatabase() {
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log("Database connected successfully" + process.env.MONGO_URL);
+  const mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
+
+  try {
+    await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      authSource: "admin", // 如果需要指定认证数据库
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1); // 如果连接失败，退出程序
+  }
 }
 
 module.exports = connectToDatabase;
